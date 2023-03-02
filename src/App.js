c3c5258c03eb1ba640fb21e9c4f99f9d560e8d1f
harisishtiaq1 <harisharry232@gmail.com>
2023-03-02 11:29:02 +0500

    Updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Sidebar from "./Components/Sidebar";
-import Drawer from "./Components/Drawer"
+
 function App() {
   return (
     <div>
-      {/* <Navbar />
-      <Sidebar /> */}
-      <Drawer/>
+      <Navbar />
+      <Sidebar />
     </div>
   );
 }
